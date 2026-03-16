aa947565f4405ba9e0aa47406f469e80cdcc7ef7
bboy <bboy@dailyview.tw>
2026-03-16 22:26:38 +0800

    fix: extend max_length in news model

--- news/models.py
+++ news/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255, blank=True)
+    title = models.CharField(max_length=500)
+    author = models.CharField(max_length=500, blank=True)
     content = models.TextField()
-    caption = models.CharField(max_length=255, blank=True)
+    caption = models.CharField(max_length=500, blank=True)
     source_url = models.URLField(unique=True)
     image_url = models.URLField(blank=True)
     news_tag = models.ManyToManyField("NewsTag", related_name="news_items", blank=True)
@@ -19,7 +19,7 @@ class News(models.Model):
 
 
 class NewsTag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
